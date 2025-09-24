efaab135991b9d000d8028b810e4c3a325429238
Voodoo30fps <voodoo30fps@gmail.com>
2025-09-24 14:14:44 +0300

    prep for Composition API

---
+++ src/App2.vue
@@ -0,0 +1,47 @@
+<script>
+export default {
+  data() {
+    return {
+      name: 'John Doe',
+      status: 'active',
+      tasks: ['Task One', 'Task Two', 'Task Three'],
+      link: 'https://google.com',
+    };
+  },
+  methods: {
+    toggleStatus() {
+      if (this.status === 'active') {
+        this.status = 'pending';
+      } else if (this.status === 'pending') {
+        this.status = 'inactive';
+      } else {
+        this.status = 'active';
+      }
+    },
+  },
+};
+</script>
+
+<template>
+  <h1>{{ name }}</h1>
+  <p v-if="status === 'active'">User is active</p>
+  <p v-else-if="status === 'pending'">User is pending</p>
+  <p v-else>User is inactive</p>
+
+  <h3>Tasks</h3>
+  <ul>
+    <li v-for="task in tasks" :key="task">{{ task }}</li>
+  </ul>
+
+  <a v-bind:href="link">Click for Google</a>
+  <!-- ^ longer version -->
+  <a :href="link">Click for Google</a>
+  <!-- ^ shorter version -->
+
+  <br />
+
+  <button v-on:click="toggleStatus">Change Status</button>
+  <!-- ^ longer version -->
+  <button @click="toggleStatus">Change Status</button>
+  <!-- ^ shorter version -->
+</template>
