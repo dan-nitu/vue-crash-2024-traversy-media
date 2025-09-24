ce4b32ebbc7d04f58209c1e367abc67d065b2c15
Voodoo30fps <voodoo30fps@gmail.com>
2025-09-24 19:13:31 +0300

    computed() & Truncate Description

--- src/components/JobListing.vue
+++ src/components/JobListing.vue
@@ -1,10 +1,22 @@
 <script setup>
-import { defineProps } from 'vue';
+import { defineProps, ref, computed } from 'vue';
 
-defineProps({
-  job: {
-    type: Object,
-  },
+const props = defineProps({
+  job: Object,
+});
+
+const showFullDescription = ref(false);
+
+const toggleFullDescription = () => {
+  showFullDescription.value = !showFullDescription.value;
+};
+
+const tuncatedDescription = computed(() => {
+  let description = props.job.description;
+  if (!showFullDescription.value) {
+    description = description.substring(0, 90) + '...';
+  }
+  return description;
 });
 </script>
 
@@ -17,7 +29,15 @@ defineProps({
       </div>
 
       <div class="mb-5">
-        {{ job.description }}
+        <div>
+          {{ tuncatedDescription }}
+        </div>
+        <button
+          @click="toggleFullDescription"
+          class="text-green-500 hover:text-green-600 mb-5"
+        >
+          {{ showFullDescription ? 'Less' : 'More' }}
+        </button>
       </div>
 
       <h3 class="text-green-500 mb-2">{{ job.salary }} / Year</h3>
