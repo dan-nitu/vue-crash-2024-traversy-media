ce1431ac5c6b207348f91ec548fdb20e8787695e
Voodoo30fps <voodoo30fps@gmail.com>
2025-09-24 18:54:23 +0300

    JobListings Component + dummy data from json file + JobListing Component

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@ import Navbar from '@/components/Navbar.vue';
 import Hero from './components/Hero.vue';
 
 import HomeCards from './components/HomeCards.vue';
+import JobListings from './components/JobListings.vue';
 </script>
 
 <template>
@@ -10,4 +11,5 @@ import HomeCards from './components/HomeCards.vue';
   <Hero />
 
   <HomeCards />
+  <JobListings />
 </template>
